e91d1bf1c2f40f4c78c97d576a54191f1063bad4
mohammad.siddique <mohammad.siddique@bank-abc.com>
2024-03-21 14:26:50 +0300

    java code

--- src/main/java/KeepMeAlive.java
+++ src/main/java/KeepMeAlive.java
@@ -4,28 +4,36 @@ import java.util.concurrent.TimeUnit;
 public class KeepMeAlive {
 
     public static void main(String[] args) {
+    	Boolean moveFlag = true;
         try {
             // Run indefinitely
             while (true) {
                 // Move the mouse cursor
-                moveMouse();
+                moveMouse(moveFlag);
+                if(moveFlag)
+                	moveFlag = false;
+                else
+                	moveFlag = true;
 
-                // Wait for a short duration
-                TimeUnit.SECONDS.sleep(30);
+                TimeUnit.SECONDS.sleep(60);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    private static void moveMouse() {
+    private static void moveMouse(Boolean moveFlag) {
         try {
             // Get the current mouse position
             Point p = MouseInfo.getPointerInfo().getLocation();
 
             // Move the mouse cursor by a small amount
             Robot robot = new Robot();
-            robot.mouseMove(p.x + 1, p.y + 1);
+            if(moveFlag)
+            	robot.mouseMove(p.x + 1, p.y + 1);
+            else
+            	robot.mouseMove(p.x - 1, p.y - 1);
+            	
         } catch (AWTException e) {
             e.printStackTrace();
         }
